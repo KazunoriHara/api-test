0fc9a1dbd0e045d7bf41142a7d0b627f38d2767f
KazunoriHara <kazunori.hara@figse.co.jp>
2021-11-18 15:24:32 +0900

    routes.rb changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   post 'users/:id/:name' => 'users#add'
   get 'lottery' => 'users#lottery'
+  root to: 'users#lottery'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
